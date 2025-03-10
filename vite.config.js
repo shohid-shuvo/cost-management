795f3d0865d337ef715ce3f5fc21c5d9cd118363
shohid-shuvo <shuvo001956@gmail.com>
2025-03-10 13:00:26 +0600

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cost-management/',
   server: {
     port:3000,
     host: '0.0.0.0',
   },
   "scripts": {
   "dev": "vite --host"
-}
+  }
 })
 
 
